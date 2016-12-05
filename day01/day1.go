d3e143e21277b8ef15ab0b422b1941d91ea0753e
Ben Phillips <ben@ben-phillips.net>
2016-12-05 21:12:21 +0000

    Process instruction.

--- day01/day1.go
+++ day01/day1.go
@@ -12,8 +12,30 @@ func main() {
 
 	var commands = strings.Split(command_string, ", ")
 
+	var x, y float64 // defaults to 0
+	var current_direction int
+
 	for _, v := range commands {
 		var direction = string(v[0])
 		var distance, _ = strconv.Atoi(string(v[1]))
+
+		if direction == "R" {
+			current_direction++
+		} else if direction == "L" {
+			current_direction += -1 + 4
+		}
+
+		current_direction %= 4
+
+		switch current_direction {
+		case 0:
+			y += float64(distance)
+		case 1:
+			x += float64(distance)
+		case 2:
+			y -= float64(distance)
+		case 3:
+			x -= float64(distance)
+		}
 	}
 }
