dc728b99f480684f273813a0156b7d93680e0bca
Ben Phillips <ben@ben-phillips.net>
2016-12-05 20:34:44 +0000

    Split the commands into a slice.

--- day01/day1.go
+++ day01/day1.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
+	"strings"
 )
 
 func main() {
 	var input, _ = ioutil.ReadFile("./day01.txt")
+	var command_string = strings.TrimSpace(string(input))
 
-	println(string(input))
+	var commands = strings.Split(command_string, ", ")
+
+	fmt.Printf("%v", commands)
 }
