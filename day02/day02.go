b02337cd10893bd52ce918ce0bf7dc90d4ca4962
Ben Phillips <ben@ben-phillips.net>
2016-12-06 06:30:38 +0000

    Cope with weird keypad shape.

--- day02/day02.go
+++ day02/day02.go
@@ -9,29 +9,40 @@ import (
 func main() {
 	var pin string
 
-	keypad := [3][3]string{
-		{"1", "2", "3"},
-		{"4", "5", "6"},
-		{"7", "8", "9"},
+	keypad := [5][5]string{
+		{"", "", "1", "", ""},
+		{"", "2", "3", "4", ""},
+		{"5", "6", "7", "8", "9"},
+		{"", "A", "B", "C", ""},
+		{"", "", "D", "", ""},
 	}
 
-	var x, y = 1, 1 // coordinates on keypad
+	var x, y = 0, 2 // coordinates on keypad
+	var temp_x, temp_y int
 
-	var input, _ = ioutil.ReadFile("./day02.txt")
+	var input, _ = ioutil.ReadFile("./day02-short.txt")
 	var command_string = strings.TrimSpace(string(input))
 	var lines = strings.Split(command_string, "\n")
 
 	for _, line := range lines {
 		for _, direction := range line {
+			temp_x = x
+			temp_y = y
+
 			switch direction {
 			case 'U':
-				y = int(math.Max(0, float64(y)-1))
+				temp_y = int(math.Max(0, float64(y)-1))
 			case 'D':
-				y = int(math.Min(2, float64(y)+1))
+				temp_y = int(math.Min(4, float64(y)+1))
 			case 'L':
-				x = int(math.Max(0, float64(x)-1))
+				temp_x = int(math.Max(0, float64(x)-1))
 			case 'R':
-				x = int(math.Min(2, float64(x)+1))
+				temp_x = int(math.Min(4, float64(x)+1))
+			}
+
+			if keypad[temp_y][temp_x] != "" {
+				x = temp_x
+				y = temp_y
 			}
 		}
 
