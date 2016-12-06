57258122f99bf7ccddff71c07846d174bbdad290
Ben Phillips <ben@ben-phillips.net>
2016-12-06 11:16:40 +0000

    Cope with vertical groups.

--- day03/day03.go
+++ day03/day03.go
@@ -8,24 +8,38 @@ import (
 	"strings"
 )
 
+func processTriangle(sides []int) {
+	sort.Ints(sides)
+
+	if sides[0]+sides[1] > sides[2] {
+		fmt.Println(sides)
+	}
+}
+
 func main() {
+	var inputs [3][3]int
+
 	var input, _ = ioutil.ReadFile("./day03.txt")
 	var command_strings = strings.TrimSpace(string(input))
 	var lines = strings.Split(command_strings, "\n")
 
-	for _, line := range lines {
-		var side_inputs = strings.Fields(line)
-		var sides []int
+	for row, line := range lines {
+		var fields = strings.Fields(line)
 
-		for _, v := range side_inputs {
+		for col, v := range fields {
 			var value, _ = strconv.Atoi(v)
-			sides = append(sides, value)
+
+			inputs[row%3][col] = value
 		}
 
-		sort.Ints(sides)
-
-		if sides[0]+sides[1] > sides[2] {
-			fmt.Println(sides)
+		if (row-2)%3 == 0 {
+			for i := 0; i < 3; i++ {
+				processTriangle([]int{
+					inputs[0][i],
+					inputs[1][i],
+					inputs[2][i],
+				})
+			}
 		}
 	}
 }
