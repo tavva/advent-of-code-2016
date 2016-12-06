77452a58ddce87efe0099bbe14e3a8c37ab139cf
Ben Phillips <ben@ben-phillips.net>
2016-12-06 06:04:04 +0000

    Move around the keypad.

--- day02/day02.go
+++ day02/day02.go
@@ -1,18 +1,35 @@
 package main
 
 import (
-	"fmt"
 	"io/ioutil"
+	"math"
 	"strings"
 )
 
 func main() {
+	keypad := [3][3]int{
+		{1, 2, 3},
+		{4, 5, 6},
+		{7, 8, 9},
+	}
+
+	var x, y = 1, 1 // coordinates on keypad
+
 	var input, _ = ioutil.ReadFile("./day02.txt")
 	var lines = strings.Split(string(input), "\n")
 
 	for _, line := range lines {
 		for _, direction := range line {
-			fmt.Printf("%v", string(direction))
+			switch direction {
+			case 'U':
+				y = int(math.Min(0, float64(y)-1))
+			case 'D':
+				y = int(math.Min(2, float64(y)+1))
+			case 'L':
+				x = int(math.Min(0, float64(x)-1))
+			case 'R':
+				x = int(math.Min(2, float64(x)+1))
+			}
 		}
 	}
 }
