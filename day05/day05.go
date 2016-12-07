372603811a22582576ded92a2713e1bbff9c8c69
Ben Phillips <ben@ben-phillips.net>
2016-12-07 05:02:31 +0000

    Use new indexing technique.

--- day05/day05.go
+++ day05/day05.go
@@ -5,22 +5,38 @@ import (
 	"encoding/hex"
 	"io"
 	"strconv"
+	"strings"
 )
 
 func main() {
 	input := "ugkcyxxp"
-	password := ""
+	password := []byte("--------")
 
 	for i := 0; i < int(^uint(0)>>1); i++ {
 		hash := md5.New()
 		io.WriteString(hash, input+strconv.Itoa(i))
 		if hex.EncodeToString(hash.Sum(nil))[:5] == "00000" {
-			password += string(hex.EncodeToString(hash.Sum(nil))[5])
-			if len(password) == 8 {
+			index := string(hex.EncodeToString(hash.Sum(nil))[5])
+			char := string(hex.EncodeToString(hash.Sum(nil))[6])
+
+			index_int, err := strconv.ParseInt(index, 0, 64)
+			if err != nil {
+				continue
+			}
+
+			if index_int >= 0 && index_int <= 7 {
+				println(index_int)
+				println(char)
+				if password[index_int] == '-' {
+					password[index_int] = hex.EncodeToString(hash.Sum(nil))[6]
+				}
+			}
+
+			println(string(password))
+
+			if !strings.Contains(string(password), "-") {
 				break
 			}
 		}
 	}
-
-	println(password)
 }
