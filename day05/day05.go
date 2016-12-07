89dcb338852fddf92d2eb0bbff210529218a751c
Ben Phillips <ben@ben-phillips.net>
2016-12-07 04:26:40 +0000

    Decode password.

--- day05/day05.go
+++ day05/day05.go
@@ -1,5 +1,26 @@
 package main
 
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"io"
+	"strconv"
+)
+
 func main() {
-	input += "ugkcyxxp"
+	input := "ugkcyxxp"
+	password := ""
+
+	for i := 0; i < int(^uint(0)>>1); i++ {
+		hash := md5.New()
+		io.WriteString(hash, input+strconv.Itoa(i))
+		if hex.EncodeToString(hash.Sum(nil))[:5] == "00000" {
+			password += string(hex.EncodeToString(hash.Sum(nil))[5])
+			if len(password) == 8 {
+				break
+			}
+		}
+	}
+
+	println(password)
 }
