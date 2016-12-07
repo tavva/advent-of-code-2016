abd6859f0dc8614ecfa08fd4c458e9f18d15efc9
Ben Phillips <ben@ben-phillips.net>
2016-12-07 03:16:43 +0000

    Sort characters by frequency.

--- day04/day04.go
+++ day04/day04.go
@@ -1,10 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
 	"regexp"
+	"sort"
 	"strings"
 )
+
+type Pair struct {
+	Key   string
+	Value int
+}
+
+type PairList []Pair
+
+func (p PairList) Len() int      { return len(p) }
+func (p PairList) Swap(i, j int) { p[i], p[j] = p[j], p[i] }
+func (p PairList) Less(i, j int) bool {
+	if p[i].Value == p[j].Value {
+		return p[1].Key < p[j].Key
+	}
+	return p[i].Value < p[j].Value
+}
 
 func main() {
 	input, _ := ioutil.ReadFile("./day04.txt")
@@ -22,6 +40,17 @@ func main() {
 		letter_counts := make(map[rune][]int)
 		for _, x := range name {
 			letter_counts[x] = append(letter_counts[x], 1)
+		}
+
+		list := make(PairList, len(letter_counts))
+
+		for k, v := range letter_counts {
+			list = append(list, Pair{string(k), len(v)})
+		}
+		sort.Sort(sort.Reverse(list))
+
+		for _, p := range list {
+			fmt.Printf("%v: %v\n", p.Key, p.Value)
 		}
 
 		println(name)
