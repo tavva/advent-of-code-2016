5933017a27eff570b73dc777b264195541217c00
Ben Phillips <ben@ben-phillips.net>
2016-12-08 03:11:08 +0000

    Skeleton for day 7.

---
+++ day07/day07.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"io/ioutil"
+	"strings"
+)
+
+func main() {
+	input, _ := ioutil.ReadFile("./day07.txt")
+	raw_lines := strings.TrimSpace(string(input))
+	lines := strings.Split(raw_lines, "\n")
+
+	for _, line := range lines {
+		println(line)
+	}
+}
