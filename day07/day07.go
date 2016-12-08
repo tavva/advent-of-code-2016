8c30d14425b95645e2dcd3248665b0ad65c52164
Ben Phillips <ben@ben-phillips.net>
2016-12-08 04:02:28 +0000

    Find aba and matching bab.

--- day07/day07.go
+++ day07/day07.go
@@ -21,6 +21,40 @@ func repeatCheck(s string) bool {
 	return false
 }
 
+func abaCheck(s string) (bool, []string) {
+	var matches []string
+
+	if len(s) < 3 {
+		return false, nil
+	}
+
+	for i := 0; i <= len(s)-3; i++ {
+		if s[i] == s[i+2] && s[i] != s[i+1] {
+			matches = append(matches, s[i:i+3])
+		}
+	}
+
+	if len(matches) > 0 {
+		return true, matches
+	}
+
+	return false, nil
+}
+
+func babCheck(s string, check string) bool {
+	if len(s) < 3 {
+		return false
+	}
+
+	for i := 0; i <= len(s)-3; i++ {
+		if s[i] == check[1] && s[i+1] == check[0] && s[i+2] == check[1] {
+			return true
+		}
+	}
+
+	return false
+}
+
 func main() {
 	input, _ := ioutil.ReadFile("./day07.txt")
 	raw_lines := strings.TrimSpace(string(input))
@@ -34,16 +68,21 @@ Line:
 		good_bits := good_re.FindAllStringSubmatch(line, -1)
 		bad_bits := bad_re.FindAllStringSubmatch(line, -1)
 
-		for _, match := range bad_bits {
-			if repeatCheck(match[1]) {
-				continue Line
-			}
-		}
-
 		for _, match := range good_bits {
-			if repeatCheck(match[1]) {
-				fmt.Println(line)
-				continue Line
+			s := match[1]
+
+			success, aba := abaCheck(s)
+			if !success {
+				continue
+			}
+
+			for _, match := range bad_bits {
+				for _, good := range aba {
+					if babCheck(match[1], good) {
+						fmt.Println(line)
+						continue Line
+					}
+				}
 			}
 		}
 	}
