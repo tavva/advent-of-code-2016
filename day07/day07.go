8bc9716529c0feca3bade944274fc50598860a20
Ben Phillips <ben@ben-phillips.net>
2016-12-08 03:41:58 +0000

    Add function to check for required repeating in a string.

--- day07/day07.go
+++ day07/day07.go
@@ -5,6 +5,20 @@ import (
 	"strings"
 )
 
+func repeatCheck(s string) bool {
+	if len(s) < 4 {
+		return false
+	}
+
+	for i := 0; i <= len(s)-4; i++ {
+		if s[i] != s[i+1] && s[i] == s[i+3] && s[i+1] == s[i+2] {
+			return true
+		}
+	}
+
+	return false
+}
+
 func main() {
 	input, _ := ioutil.ReadFile("./day07.txt")
 	raw_lines := strings.TrimSpace(string(input))
