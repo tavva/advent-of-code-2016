9739fa9d8cae3d2b733e4ab773fa8724ce7c83c6
Ben Phillips <ben@ben-phillips.net>
2016-12-08 10:19:26 +0000

    Initialise a screen.

--- day08/day08.go
+++ day08/day08.go
@@ -6,6 +6,8 @@ import (
 )
 
 func main() {
+	var screen [50][6]int
+
 	input, _ := ioutil.ReadFile("./day08.txt")
 	raw_lines := strings.TrimSpace(string(input))
 	lines := strings.Split(raw_lines, "\n")
